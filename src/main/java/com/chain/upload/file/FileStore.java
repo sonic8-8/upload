c683bb317b6b1cf946e8e3457fdc85217de5687b
이승환 <seaung13@gmail.com>
2025-01-10 02:08:08 +0900

    feat: 스프링과 파일 업로드 예제 - 상품 이름, 첨부파일 1개, 이미지 파일 여러개의 업로드 및 다운로드 기능, 뷰 템플릿 추가

---
+++ src/main/java/com/chain/upload/file/FileStore.java
@@ -0,0 +1,59 @@
+package com.chain.upload.file;
+
+import com.chain.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String fileName) {
+        return fileDir + fileName;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String extension = extractExtension(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + extension;
+
+    }
+
+    private String extractExtension(String originalFileName) {
+        int position = originalFileName.lastIndexOf(".");
+        return originalFileName.substring(position + 1);
+    }
+
+}
